7ea6719ab1c9bdf84c28eab855cd83b174bcbc71
Chen Gao <gaochen315@users.noreply.github.com>
2021-08-26 13:34:31 -0400

    add --no-check-certificate flag for downloading files from the VT filebox.

--- download_data_weights.sh
+++ download_data_weights.sh
@@ -1,8 +1,8 @@
 
-wget https://filebox.ece.vt.edu/~chengao/FGVC/data.zip
+wget --no-check-certificate https://filebox.ece.vt.edu/~chengao/FGVC/data.zip
 unzip data.zip
 rm data.zip
 
-wget https://filebox.ece.vt.edu/~chengao/FGVC/weight.zip
+wget --no-check-certificate https://filebox.ece.vt.edu/~chengao/FGVC/weight.zip
 unzip weight.zip
 rm weight.zip
